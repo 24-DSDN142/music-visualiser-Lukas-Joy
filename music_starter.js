7a3b5774d098d8e9bf492bd82f0867665d55c4e6
Lukas-Joy <lukasjoy67@gmail.com>
2024-09-08 18:01:42 +1200

    attempt at hisotr circle

--- music_starter.js
+++ music_starter.js
@@ -1,17 +1,36 @@
-let Horse;
-let firstRun = true
 let numberOfTimes = 20 //Changes the Number of times the 4 audio leevels are dsipalyed 
-let star = false
 let smoothingValue = 0
 let words_history = [];
 let vocal_history = [];
 let drum_history = [];
 let bass_history = [];
 let other_history = [];
+let AudioLevelPointVocal = [];
+let AudioLevelPointDrum = [];
+let AudioLevelPointBass = [];
+let AudioLevelPointOther = [];
+let vocal_historyModified = [];
+let drum_historyModified = [];
+let bass_historyModified = [];
+let other_historyModified = []; 
 
+
+function draw_history_line(history) {
+  beginShape();
+  vertex(0,canvasY);
+  for(let i=0; i<history.length; i++) {
+    vertex(i*4, map(history[i], 0, 100, height, height/8, true));
+  }
+  vertex(canvasX,canvasY);
+  endShape();
+}
 function add_to_history(history, d) {
   history.push(d);
+  if(history.length >= (width-1)/4) {
+    history.shift();
+  }
 }
+
 
 function generateNoiseGrid(gridSize) {
   for (let x = 0; x < width; x += gridSize) {
@@ -27,71 +46,63 @@ function generateNoiseGrid(gridSize) {
 }
 
 
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
-function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  // console.log(counter)
-  background(150)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  if(firstRun){
-    Horse = loadImage('Horse.png');
-      firstRun = false
+function donutVisualCurved(centerX, centerY,circleRadius,min,max,colour){
+  let maximum = (centerY - (max * circleRadius));
+  let minimum = (centerY - (min * circleRadius));
+  
+  let vocalMap = map(vocal,0,100,minimum,maximum);
+  let drumMap = map(drum,0,100,minimum,maximum);
+  let bassMap = map(bass,0,100,minimum,maximum);
+  let otherMap = map(other,0,100,minimum,maximum);
+  
+  let offsetAngle = (360/(4*numberOfTimes));
+  
+  const AudioLevelPoint = [];
+  
+  let AudioType = [vocalMap, drumMap, bassMap, otherMap];
+  
+  for(let i = 0; i < 4*numberOfTimes; i+=4){
+    for(let x = 0; x < 4; x++){
+    AudioLevelPoint.push([ // outer ring
+      (centerX - centerX) * cos((i + x) * offsetAngle) - (AudioType[x] - centerY) * sin((i + x) * offsetAngle) + centerX,
+      (centerX - centerX) * sin((i + x) * offsetAngle) + (AudioType[x] - centerY) * cos((i + x) * offsetAngle) + centerY
+      ])
+    } 
   }
-
-add_to_history(words_history, words);
-add_to_history(vocal_history, vocal);
-add_to_history(drum_history, drum);
-add_to_history(bass_history, bass);
-add_to_history(other_history, other);
-
-if(counter <= 480){
-generateNoiseGrid(20); 
-blackScreen1 = map(other,65,66,255,0);
-fill(0,blackScreen1);
-rect(0,0,2*canvasX,2*canvasY);
+  
+  fill(colour); // outer ring
+  beginShape();
+      for(let i = 0; i < 4*numberOfTimes; i++){
+        curveVertex(AudioLevelPoint[i][0],AudioLevelPoint[i][1]);
+      }
+      curveVertex(AudioLevelPoint[0][0],AudioLevelPoint[0][1]);
+      curveVertex(AudioLevelPoint[1][0],AudioLevelPoint[1][1]);
+  endShape();
 }
 
-if(counter >= 480 && counter <= 2640){
-  smoothingValue = 0
-  donutVisualCurved(canvasX/2, canvasY/2,1000,0.2,1);
-}
+// vocal, drum, bass, and other are volumes ranging from 0 to 100
+function draw_one_frame(words, vocal, drum, bass, other, counter){
+  // console.log(counter)
+  background(255);
+  add_to_history(words_history, words);
+  add_to_history(vocal_history, vocal);
+  add_to_history(drum_history, drum);
+  add_to_history(bass_history, bass);
+  add_to_history(other_history, other);
+ 
+// if(counter <= 480){
+// generateNoiseGrid(20); 
+// blackScreen1 = map(other,65,66,255,0);
+// fill(0,blackScreen1);
+// rect(0,0,2*canvasX,2*canvasY);
+// }
 
+// if(counter >= 480 && counter <= 2640){
 
-function donutVisual(centerX, centerY,circleRadius,min,max){
+strokeWeight(10);  
 
-let maximum = (centerY - (max * circleRadius));
-let minimum = (centerY - (min * circleRadius));
-
-let vocalMap = map(vocal,0,100,minimum,maximum);
-let drumMap = map(drum,0,100,minimum,maximum);
-let bassMap = map(bass,0,100,minimum,maximum);
-let otherMap = map(other,0,100,minimum,maximum);
-
-let offsetAngle = (360/(4*numberOfTimes));
-
-const AudioLevelPoint = [];
-
-let AudioType = [vocalMap, drumMap, bassMap, otherMap];
-
-for(let i = 0; i < 4*numberOfTimes; i+=4){
-  for(let x = 0; x < 4; x++){
-  AudioLevelPoint.push([ // outer ring
-    (centerX - centerX) * cos((i + x) * offsetAngle) - (AudioType[x] - centerY) * sin((i + x) * offsetAngle) + centerX,
-    (centerX - centerX) * sin((i + x) * offsetAngle) + (AudioType[x] - centerY) * cos((i + x) * offsetAngle) + centerY
-    ])
-  } 
-}
-
-fill(20,243,127); // outer ring
-beginShape();
-    for(let i = 0; i < 4*numberOfTimes; i++){
-      vertex(AudioLevelPoint[i][0],AudioLevelPoint[i][1]);
-    }
-    vertex(AudioLevelPoint[0][0],AudioLevelPoint[0][1]);
-endShape();
-}
-function donutVisualCurved(centerX, centerY,circleRadius,min,max){
+donutVisual(canvasX/2,canvasY/2,500,0,100,[213,32,132]);
+function donutVisual(centerX, centerY,circleRadius,min,max,colour){
 
   let maximum = (centerY - (max * circleRadius));
   let minimum = (centerY - (min * circleRadius));
@@ -116,14 +127,19 @@ function donutVisualCurved(centerX, centerY,circleRadius,min,max){
     } 
   }
   
-  fill(20,243,127); // outer ring
+  fill(colour); // outer ring
   beginShape();
       for(let i = 0; i < 4*numberOfTimes; i++){
-        curveVertex(AudioLevelPoint[i][0],AudioLevelPoint[i][1]);
+        vertex(AudioLevelPoint[i][0],AudioLevelPoint[i][1]);
       }
-      curveVertex(AudioLevelPoint[0][0],AudioLevelPoint[0][1]);
+      vertex(AudioLevelPoint[0][0],AudioLevelPoint[0][1]);
   endShape();
   }
-
 }
 
+function reset_music() {
+  vocal_history = [];
+  drum_history = [];
+  bass_history = [];
+  other_history = [];
+}
